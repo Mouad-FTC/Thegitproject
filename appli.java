c8279c3b922e9fcca00ead025f19358d5d9bae11
Mouad <mouadenasiry84@gmail.com>
2024-08-27 23:54:01 +0100

    Updating the java file

--- appli.java
+++ appli.java
@@ -1,7 +1,7 @@
 public class appli {
 
     public static void main(String[] args) {
-        System.out.println("Salam new version !");
+        System.out.println("This is an update !");
         System.out.println("Ahlan");
     }
 }
